a58264fd3e8874741659d29daa685bfafd4d0b56
Dmitry <88534324+skaska1x1@users.noreply.github.com>
2022-09-03 20:02:16 +0300

    server change

--- src/server.js
+++ src/server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('./'));
+app.use(express.static('/dist'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/', 'index.pug'))
+    res.sendFile(path.join(__dirname, '/dist', 'index.html'))
 })
 
 app.listen(PORT, function () {
