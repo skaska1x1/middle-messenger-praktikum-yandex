cc91dacb3226c9354f81b74c62a3861def87abb9
skaska1x1 <skaska1x1@ya.ru>
2022-09-03 20:12:51 +0300

    components change

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './style.css';
-import './pages/login/login.css'
+import './pages/login/login.css';
+import './components/button/style.css';
+import './components/input/style.css';
+import './components/link/style.css';
